65c94e45379afba28a78ca567a45e767bd452756
yyyywm <a1969334998@hotmail.com>
2026-04-26 02:32:10 +0800

    chore(android): rename package to com.pennytrack.app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.bookeep"
+    namespace = "com.pennytrack.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.bookeep"
+        applicationId = "com.pennytrack.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/pennytrack/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/pennytrack/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.bookeep
+package com.pennytrack.app
 
 import io.flutter.embedding.android.FlutterActivity
 
